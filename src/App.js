2ad56a7eaaf1c508eab94635b22ffc60418e0322
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-07-31 07:59:48 +0300

    15. added react-toastify

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import AppRouter from "./router/AppRouter";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import { GlobalStyles } from "./styles/Global.styles";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <AppRouter />
+        <ToastContainer />
       </ThemeProvider>
     </div>
   );
